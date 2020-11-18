7f4c3339b1617e6fca8ab1ac6b6a079d981b0300
Amir Hallak <61103915+amirhallak@users.noreply.github.com>
2020-11-17 23:52:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Cover Letter Creator
+
+This code makes it easier for the job searcher to quickly generate a PDF of his or her cover letter. It assumes that the bulk of the cover letter is already written and the main changes are often the company name, location of a company, the position they're applying for, and date. 
+
+
+
+# Table of Contents
+* [Requirments](#Requirments)
+* [Imports](#Imports)
+* [Configuration](#Configuration)
+* [Installation/Usage](#Installation/Usage)
+* [Screenshots](#Screenshots)
+
+
+# Requirments
+Python 3.7.5 64-bit or equivalant.
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install [FPDF](https://pyfpdf.readthedocs.io/en/latest/). 
+```bash
+pip install FPDF
+```
+
+# Imports
+```python
+from fpdf import FPDF
+from datetime import datetime
+```
+
+# Configuration
+Write the body of your cover letter before hand and store the changes in the **cover_text.txt** file. 
+
+Make sure when writing the body of the text in **cover_text.txt**:
+
+1. Replace the company positon with {0}
+2. Replace the company name with {1}
+
+This is so the script can successfully replace the information the user input into their correct string slots. 
+
+
+# Installation/Usage
+
+### Step 1
+* :running: Clone this repo to your local machine using: 
+
+```bash
+git clone https://github.com/amirhallak/Cover_Letter
+```
+
+### Step 2
+* Make sure to follow the instructions in the [Configuration](#Configuration) section. 
+
+### Step 3
+* Run the script in your terminal:
+```bash
+python cover_code.py
+```
+### Step 4
+* Input the information regarding the application and enjoy your newly exported cover letter! 
+
+Check the [Screenshots](#Screenshots) for a visual demonstration.
+
+# Screenshots 
+* When the script is run in terminal, you will see the following output, in which you fill in your information:
+
+![Alt text](./Screenshots/Screenshot_1.png)
+
+* The corresponding output for the above input information:
+
+![Alt text](./Screenshots/Screenshot_2.png)
+
+**_It is that simple!_**
---
+++ cover_code.py
@@ -0,0 +1,102 @@
+'''
+This is a Python code to edit cover letter based on the job position and company name.
+
+@author Amir Hallak
+'''
+
+###########################################################
+###########################################################
+
+# import modules
+from fpdf import FPDF
+from datetime import datetime
+
+###########################################################
+###########################################################
+
+# constants
+TEXT_FILE = './cover_text.txt'
+FONT = 'Times'
+FONT_SIZE = 12
+UNIT = 'mm'
+FORMAT = 'letter'
+
+###########################################################
+###########################################################
+
+# retrieving application information 
+def company_info():
+
+    global applicant_name, date
+    global company_name, company_position, company_location
+    
+    applicant_name = input("Enter your full name: ")
+    company_name = input("Enter company name: ")
+    company_position = input("Enter company positon: ")
+    company_location = input("Enter company location: ")
+    date = datetime.today().strftime('%B %d, %Y')
+
+###########################################################
+###########################################################
+
+# header specifications in the pdf
+def pdf_header(document):
+
+    # header text
+    header_text = f'{date}\n{company_name}\n{company_location}\nPosition: {company_position}'
+
+    # writing header information
+    document.set_font(FONT, 'B', FONT_SIZE)
+    document.multi_cell(0,5, header_text, ln=2)
+    
+# body specifications and body text import
+def pdf_body(document):
+
+    # cover text
+    with open(TEXT_FILE, 'rb') as F1:
+        body_text = F1.read().decode('latin1').format(company_position, company_name)
+
+    # writing body
+    document.ln(12.5)
+    document.set_font(FONT, '', FONT_SIZE)
+    document.multi_cell(0,5, body_text, ln=2)
+
+# footer specifications in the pdf
+def pdf_footer(document):
+
+    # footer text
+    footer_text = f'Thank you,\n{applicant_name}'
+    
+    # writing footer
+    document.ln(12.5)
+    document.set_font(FONT, 'B', FONT_SIZE)
+    document.multi_cell(0, 5, footer_text)
+    
+###########################################################
+###########################################################
+
+# writing pdf
+def pdf_writer():
+    '''
+    The actual PDF writer starts.
+    '''
+    
+    # setting up page settings
+    document = FPDF()
+    document.set_margins(25.4, 25.4, 25.4)
+    document.add_page()
+    
+    pdf_header(document)
+    pdf_body(document)
+    pdf_footer(document)
+
+    # export
+    document.output(f"./Hallak_Cover_{company_name}.pdf")
+
+###########################################################
+###########################################################
+
+#run
+if __name__ == "__main__":
+    company_info()
+    pdf_writer()
